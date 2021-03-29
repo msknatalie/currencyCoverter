d0f7b7f3bc4ed77a9744bf345940b83b5731cabb
msknatalie <cgs.korneeva@gmail.com>
2021-03-29 21:43:27 +0300

    Stop for 29.03.21

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # currencyCoverter
 Algoritmika, Project M3, Currency Converter
+
+1. Сначала сделать конвертацию только с инпутами
+2. Добавить реакцию на выбор валюты
---
+++ script.js
@@ -0,0 +1,8 @@
+const inputSell = document.querySelector('.sell');
+const inputBuy = document.querySelector('.buy');
+
+inputSell.addEventListener('change', updateValue); //change or keyup
+
+function updateValue (e) {
+  
+}
