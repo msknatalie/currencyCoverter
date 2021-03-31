0800bb22accd1efc4763b8612ff6b228237f86a4
msknatalie <cgs.korneeva@gmail.com>
2021-03-31 12:47:49 +0300

    js problems

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const inputSell = document.querySelector('.sell');
 const inputBuy = document.querySelector('.buy');
 
-inputSell.addEventListener('change', updateValue); //change or keyup
+//inputSell.addEventListener('keyup', updateValue); // изменение инпута
 
-function updateValue (e) {
+const BASE_URL = "https://api.ratesapi.io/api";
+const rub = "RUB";
+const usd = "USD";
+const eur = "EUR";
+const grb = "GRB";
+
+async function converter (cur1, cur2) {
+    const response = await fetch(`${BASE_URL}/latest?base=${cur1}&symbols=${cur2}`);
+    const data = await response.json();
+    return data;
+// console.log(data); //проверка - Ок
+// console.log(data.rates.USD); //проверка - Ок
+  }
+
+const currencyButton = document.querySelector("#myRub");
+
+currencyButton.addEventListener("click", () => {
+    currencyButton.style.background = "#833AE0";
+    converter (rub, usd);
+    console.log(data); // не работает
+    let usdRate = data.rates.USD;
+    inputBuy.value = inputSell.value * usdRate;
+})
   
-}
+//function updateValue (e) {
+//    inputBuy.value = e.target.value;
+//}
