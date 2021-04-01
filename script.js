7c81e7530834890078d32179c184fbf7be79ce39
msknatalie <cgs.korneeva@gmail.com>
2021-04-01 13:00:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const inputSell = document.querySelector('.sell');
-const inputBuy = document.querySelector('.buy');
-
-//inputSell.addEventListener('keyup', updateValue); // изменение инпута
+const inputSell = document.querySelector(".sell");
+const inputBuy = document.querySelector(".buy");
 
 const BASE_URL = "https://api.ratesapi.io/api";
 const rub = "RUB";
@@ -9,24 +7,74 @@ const usd = "USD";
 const eur = "EUR";
 const grb = "GRB";
 
-async function converter (cur1, cur2) {
-    const response = await fetch(`${BASE_URL}/latest?base=${cur1}&symbols=${cur2}`);
-    const data = await response.json();
-    return data;
-// console.log(data); //проверка - Ок
-// console.log(data.rates.USD); //проверка - Ок
-  }
+async function converter(cur1, cur2) {
+  const response = await fetch(
+    `${BASE_URL}/latest?base=${cur1}&symbols=${cur2}`
+  );
+  const data = await response.json();
+  return data;
+  // console.log(data); //проверка - Ок
+  // console.log(data.rates.USD); //проверка - Ок
+}
 
-const currencyButton = document.querySelector("#myRub");
+// Кнопки первого инпута:
+const currencyButtonRub = document.querySelector("#myRub");
+const currencyButtonUsd = document.querySelector("#myUsd");
+const currencyButtonEur = document.querySelector("#myEur");
+const currencyButtonGbp = document.querySelector("#myGbp");
 
-currencyButton.addEventListener("click", () => {
-    currencyButton.style.background = "#833AE0";
-    converter (rub, usd);
-    console.log(data); // не работает
-    let usdRate = data.rates.USD;
+// Кнопки второго инпута:
+const currencyButRub = document.querySelector("#rub");
+const currencyButUsd = document.querySelector("#usd");
+const currencyButEur = document.querySelector("#eur");
+const currencyButGbp = document.querySelector("#gbp");
+
+// Загрузка страницы
+window.addEventListener("load", () => {
+  currencyButtonRub.style.background = "#833AE0";
+  currencyButUsd.style.background = "#833AE0";
+  inputSell.value = 1;
+  let result = converter(rub, usd);
+  result.then((rate) => {
+    let usdRate = rate.rates.USD;
     inputBuy.value = inputSell.value * usdRate;
-})
-  
-//function updateValue (e) {
-//    inputBuy.value = e.target.value;
-//}
+  });
+  inputSell.addEventListener("keyup", updateValue);
+});
+
+//inputSell.addEventListener("keyup", updateValue); // изменение инпута
+function updateValue(e) {
+  let result = converter(rub, usd);
+  result.then((rate) => {
+    let usdRate = rate.rates.USD;
+    inputBuy.value = e.target.value * usdRate;
+  });
+}
+
+// Конвертация
+// Если в первом инпуте выбран рубль:
+//currencyButtonRub.addEventListener("click", () => {
+//    currencyButtonRub.style.background = "#833AE0";
+//    currencyButUsd.addEventListener("click", () => {
+//      currencyButUsd.style.background = "#833AE0";
+//      let result = converter(rub, usd);
+//      result.then((rate) => {
+//        console.log(rate); // Ok
+//        console.log(rate.rates.USD); // Ok
+//        let usdRate = rate.rates.USD;
+//        inputBuy.value = inputSell.value * usdRate;
+//        return inputBuy.value; // Ok
+//      });
+//    });
+//    currencyButEur.addEventListener("click", () => {
+//      currencyButEur.style.background = "#833AE0";
+//      let result = converter(rub, eur);
+//      result.then((rate) => {
+//        console.log(rate); // Ok
+//        console.log(rate.rates.EUR); // Ok
+//        let eurRate = rate.rates.EUR;
+//        inputBuy.value = inputSell.value * eurRate;
+//        return inputBuy.value; // Ok
+//      });
+//    })
+//})
