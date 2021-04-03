a3628c92018b5f62fb96cf4a1818762bf60bfce2
msknatalie <cgs.korneeva@gmail.com>
2021-04-03 12:40:05 +0300

    function problems

--- script.js
+++ script.js
@@ -5,29 +5,34 @@ const BASE_URL = "https://api.ratesapi.io/api";
 const rub = "RUB";
 const usd = "USD";
 const eur = "EUR";
-const grb = "GRB";
+const gbp = "GBP";
+let firstCurrency = rub;
+let secondCurrency = usd;
+let curRate;
 
 async function converter(cur1, cur2) {
   const response = await fetch(
     `${BASE_URL}/latest?base=${cur1}&symbols=${cur2}`
   );
   const data = await response.json();
-  return data;
-  // console.log(data); //проверка - Ок
-  // console.log(data.rates.USD); //проверка - Ок
+  let result = data; // не было
+  result.then((rate) => {
+  curRate = rate.rates[cur2];
+  return curRate;
+})
 }
+
+function updateValue(e) {
+    inputBuy.value = e.target.value * curRate;
+  }
 
 // Кнопки первого инпута:
 const currencyButtonRub = document.querySelector("#myRub");
-const currencyButtonUsd = document.querySelector("#myUsd");
-const currencyButtonEur = document.querySelector("#myEur");
-const currencyButtonGbp = document.querySelector("#myGbp");
+const currencyButtonFrom = document.querySelectorAll(".currencyFrom");
 
 // Кнопки второго инпута:
-const currencyButRub = document.querySelector("#rub");
 const currencyButUsd = document.querySelector("#usd");
-const currencyButEur = document.querySelector("#eur");
-const currencyButGbp = document.querySelector("#gbp");
+const currencyButtonTo = document.querySelectorAll(".currencyTo");
 
 // Загрузка страницы
 window.addEventListener("load", () => {
@@ -39,42 +44,66 @@ window.addEventListener("load", () => {
     let usdRate = rate.rates.USD;
     inputBuy.value = inputSell.value * usdRate;
   });
-  inputSell.addEventListener("keyup", updateValue);
 });
 
-//inputSell.addEventListener("keyup", updateValue); // изменение инпута
-function updateValue(e) {
-  let result = converter(rub, usd);
-  result.then((rate) => {
-    let usdRate = rate.rates.USD;
-    inputBuy.value = e.target.value * usdRate;
-  });
-}
+// изменение инпута
+inputSell.addEventListener("keyup", updateValue);
 
-// Конвертация
-// Если в первом инпуте выбран рубль:
-//currencyButtonRub.addEventListener("click", () => {
-//    currencyButtonRub.style.background = "#833AE0";
-//    currencyButUsd.addEventListener("click", () => {
-//      currencyButUsd.style.background = "#833AE0";
-//      let result = converter(rub, usd);
-//      result.then((rate) => {
-//        console.log(rate); // Ok
-//        console.log(rate.rates.USD); // Ok
-//        let usdRate = rate.rates.USD;
-//        inputBuy.value = inputSell.value * usdRate;
-//        return inputBuy.value; // Ok
-//      });
-//    });
-//    currencyButEur.addEventListener("click", () => {
-//      currencyButEur.style.background = "#833AE0";
-//      let result = converter(rub, eur);
-//      result.then((rate) => {
-//        console.log(rate); // Ok
-//        console.log(rate.rates.EUR); // Ok
-//        let eurRate = rate.rates.EUR;
-//        inputBuy.value = inputSell.value * eurRate;
-//        return inputBuy.value; // Ok
-//      });
-//    })
-//})
+currencyButtonFrom.forEach((currencyFrom) => {
+    currencyFrom.addEventListener("click", () => {
+      switch (currencyFrom.innerText) {
+        case "RUB":
+          currencyFrom.style.background = "#833AE0";
+          firstCurrency = rub;
+          converter(firstCurrency, secondCurrency);
+          break
+        case "USD":
+          currencyFrom.style.background = "#833AE0";
+          firstCurrency = usd;
+          converter(firstCurrency, secondCurrency);
+          break
+        case "EUR":
+          currencyFrom.style.background = "#833AE0";
+          firstCurrency = eur;
+          converter(firstCurrency, secondCurrency);
+          break
+        case "GBP":
+          currencyFrom.style.background = "#833AE0";
+          firstCurrency = gbp;
+          converter(firstCurrency, secondCurrency);
+          break
+      }
+      return firstCurrency;
+    });
+  //  converter(firstCurrency, secondCurrency);
+  });
+
+inputBuy.addEventListener("keyup", updateValue);
+
+currencyButtonTo.forEach((currencyTo) => {
+    currencyTo.addEventListener("click", () => {
+      switch (currencyTo.innerText) {
+        case "RUB":
+          currencyTo.style.background = "#833AE0";
+          secondCurrency = rub;
+          converter(firstCurrency, secondCurrency);
+          break
+        case "USD":
+          currencyTo.style.background = "#833AE0";
+          secondCurrency = usd;
+          converter(firstCurrency, secondCurrency);
+          break
+        case "EUR":
+          currencyTo.style.background = "#833AE0";
+          secondCurrency = eur;
+          converter(firstCurrency, secondCurrency);
+          break
+        case "GBP":
+          currencyTo.style.background = "#833AE0";
+          secondCurrency = gbp;
+          converter(firstCurrency, secondCurrency);
+          break
+      }
+      return secondCurrency;
+    });
+  })
