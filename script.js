baa8ddf593167b24bccf5f899d6fd857f0282a04
msknatalie <cgs.korneeva@gmail.com>
2021-04-03 19:13:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,17 @@ async function converter(cur1, cur2) {
     `${BASE_URL}/latest?base=${cur1}&symbols=${cur2}`
   );
   const data = await response.json();
-  let result = data; // не было
-  result.then((rate) => {
-  curRate = rate.rates[cur2];
+  curRate = data.rates[cur2];
+  inputBuy.value = inputSell.value * curRate;
   return curRate;
-})
 }
 
-function updateValue(e) {
+function updateBuyValue(e) {
     inputBuy.value = e.target.value * curRate;
+  }
+
+  function updateSellValue(e) {
+    inputSell.value = e.target.value / curRate;
   }
 
 // Кнопки первого инпута:
@@ -39,71 +41,50 @@ window.addEventListener("load", () => {
   currencyButtonRub.style.background = "#833AE0";
   currencyButUsd.style.background = "#833AE0";
   inputSell.value = 1;
-  let result = converter(rub, usd);
-  result.then((rate) => {
-    let usdRate = rate.rates.USD;
-    inputBuy.value = inputSell.value * usdRate;
-  });
-});
+  converter(firstCurrency, secondCurrency);
+  })
 
-// изменение инпута
-inputSell.addEventListener("keyup", updateValue);
+// Изменение инпута
+inputSell.addEventListener("keyup", updateBuyValue);
+inputBuy.addEventListener("keyup", updateSellValue);
 
+// Кнопки валют
 currencyButtonFrom.forEach((currencyFrom) => {
     currencyFrom.addEventListener("click", () => {
       switch (currencyFrom.innerText) {
         case "RUB":
           currencyFrom.style.background = "#833AE0";
-          firstCurrency = rub;
-          converter(firstCurrency, secondCurrency);
-          break
+          return firstCurrency = rub;
         case "USD":
           currencyFrom.style.background = "#833AE0";
-          firstCurrency = usd;
-          converter(firstCurrency, secondCurrency);
-          break
+          return firstCurrency = usd;
         case "EUR":
           currencyFrom.style.background = "#833AE0";
-          firstCurrency = eur;
-          converter(firstCurrency, secondCurrency);
-          break
+          return firstCurrency = eur;
         case "GBP":
           currencyFrom.style.background = "#833AE0";
-          firstCurrency = gbp;
-          converter(firstCurrency, secondCurrency);
-          break
+          return firstCurrency = gbp;
       }
-      return firstCurrency;
+      converter(firstCurrency, secondCurrency);
     });
-  //  converter(firstCurrency, secondCurrency);
   });
-
-inputBuy.addEventListener("keyup", updateValue);
 
 currencyButtonTo.forEach((currencyTo) => {
     currencyTo.addEventListener("click", () => {
       switch (currencyTo.innerText) {
         case "RUB":
           currencyTo.style.background = "#833AE0";
-          secondCurrency = rub;
-          converter(firstCurrency, secondCurrency);
-          break
+          return secondCurrency = rub;
         case "USD":
           currencyTo.style.background = "#833AE0";
-          secondCurrency = usd;
-          converter(firstCurrency, secondCurrency);
-          break
+          return secondCurrency = usd;
         case "EUR":
           currencyTo.style.background = "#833AE0";
-          secondCurrency = eur;
-          converter(firstCurrency, secondCurrency);
-          break
+          return secondCurrency = eur;
         case "GBP":
           currencyTo.style.background = "#833AE0";
-          secondCurrency = gbp;
-          converter(firstCurrency, secondCurrency);
-          break
+          return secondCurrency = gbp;
       }
-      return secondCurrency;
+      converter(firstCurrency, secondCurrency);
     });
   })
